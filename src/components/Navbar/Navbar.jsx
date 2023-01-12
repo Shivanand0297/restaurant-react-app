4c77d47f732e944fa1145a659391db48fb104e2a
Shivanand <Shivanand0297@gmail.com>
2023-01-12 20:11:28 +0530

    refactored some code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,9 @@ import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <img src={images.gericht} alt="app logo" />
-      </div>
 
-      <ul className="app__navbar-links">
+  const ListItems = () =>(
+    <>
         <li className="p__opensans">
           <a href="#home">Home</a>
         </li>
@@ -27,6 +23,17 @@ const Navbar = () => {
         <li className="p__opensans">
           <a href="#contact">Contact</a>
         </li>
+    </>
+  )
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+
+      <ul className="app__navbar-links">
+        <ListItems/>
       </ul>
 
       <div className="app__navbar-login">
@@ -55,21 +62,7 @@ const Navbar = () => {
             onClick={()=>{setToggle(false)}} 
             />
             <ul className="app__navbar-smallscreen-links">
-              <li className="p__opensans">
-                <a href="#home">Home</a>
-              </li>
-              <li className="p__opensans">
-                <a href="#about">About</a>
-              </li>
-              <li className="p__opensans">
-                <a href="#menu">Menu</a>
-              </li>
-              <li className="p__opensans">
-                <a href="#awards">Awards</a>
-              </li>
-              <li className="p__opensans">
-                <a href="#contact">Contact</a>
-              </li>
+            <ListItems/>
             </ul>
           </div>
         )}

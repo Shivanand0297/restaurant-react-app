0f410b54a31c3405613aeeb198d046b0833fb1c7
Shivanand <Shivanand0297@gmail.com>
2023-01-14 00:32:47 +0530

    className

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -7,25 +7,25 @@ import './Chef.css';
 const Chef = () => (
   <div className='app__bg app__wrapper section__padding' >
 
-    <div class="app__wrapper_img app__wrapper_img-reverse">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={assets.chef} alt="chej"/>
     </div>
 
-    <div class="app__wrapper_info">
+    <div className="app__wrapper_info">
       <SubHeading title={"Chef's Word"} />
-      <h1 class="headtext__cormorant">What we believe in</h1>
+      <h1 className="headtext__cormorant">What we believe in</h1>
 
-      <div class="app__chef-content">
-        <div class="app__chef-content_quote">
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
           <img src={assets.quote} alt="quote"/>
           <p className='p__opensans' >Lorem ipsum dolor sit amet consectetur, adipisicing.</p>
         </div>
           <p className='p__opensans' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet dignissimos accusantium delectus perspiciatis quaerat dolores natus molestiae est fugiat impedit.</p>
       </div>
 
-      <div class="app__chef-sign">
+      <div className="app__chef-sign">
         <p>Kevin luo</p>
-        <p class="p__opensans">Chef & Founder</p>
+        <p className="p__opensans">Chef & Founder</p>
         <img src={assets.sign} alt="sign"/>
       </div>
       

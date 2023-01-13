428e005fc0a08b049cef632a1d56bf3058bc66d2
Shivanand <Shivanand0297@gmail.com>
2023-01-13 22:33:01 +0530

    added mouse enter and mouse leave functionality

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
 import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs"
+import { meal } from '../../constants';
+const Intro = () => {
 
-const Intro = () => (
-  <div>
-    Intro
+  const [playVideo, setPlayVideo] = useState(false)
+  const videoRef = useRef() //using useref to take the raference of the video dom element whose .current property we can manepulate later on
+
+  const handleVideo = () =>{
+    setPlayVideo(!playVideo)
+    console.log(playVideo);
+    if(playVideo){
+      videoRef.current.pause()
+
+    }else{
+      videoRef.current.play()
+    }
+  }
+
+  return(
+  <div className='app__video' 
+  onMouseEnter={()=>{
+    const overlay = document.querySelector("#overlay")
+    overlay.classList.add("app__video-overlay")
+    const playbutton = document.querySelector(".app__video-overlay_circle")
+    playbutton.classList.add("flex__center")
+  }}
+  onMouseLeave={()=>{
+    const overlay = document.querySelector("#overlay")
+    overlay.classList.remove("app__video-overlay")
+    const playbutton = document.querySelector(".app__video-overlay_circle")
+    playbutton.classList.remove("flex__center")  
+  }}
+  >
+    
+    <video 
+    src={meal}
+    loop
+    controls={false}
+    muted
+    ref={videoRef}
+    />
+
+    <div class="flex__center" id='overlay'
+    >
+      <div class="app__video-overlay_circle hidden"
+      onClick={handleVideo}
+    >
+      {playVideo 
+      ? <BsPauseFill color='#fff' fontSize={30} /> 
+      
+      : <BsFillPlayFill color='#fff' fontSize={30} /> }
+      </div>
+    </div>
+
   </div>
-);
+  )
+
+}
 
 export default Intro;
